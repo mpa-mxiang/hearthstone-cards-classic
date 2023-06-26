bce0795a206f1d4714178dd0d520ee169d14b06a
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-26 15:22:10 -0400

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MetricList from '../components/MetricList';
+
+const Home = () => {
+  return (
+    <div>
+      <MetricList />
+    </div>
+  );
+};
+
+export default Home;
