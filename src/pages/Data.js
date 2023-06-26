de25f4634d40613d8ac21ee3253429c50356e3d6
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-06-26 15:22:08 -0400

    Create Data.js

---
+++ src/pages/Data.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MetricData from '../components/MetricData';
+
+const Data = ({ match }) => {
+  return (
+    <div>
+      <MetricDetails match={match} />
+    </div>
+  );
+};
+
+export default Data;
